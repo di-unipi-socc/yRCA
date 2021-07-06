4ea488e5478604664fed7eaca0c80c50fda24ef8
Stefano <stefano.forti92@gmail.com>
2021-07-06 17:41:41 +0200

    first prototype

--- explain.pl
+++ explain.pl
@@ -28,7 +28,7 @@ log(s1,'error',err,999).
 log(s2,'alive',info,200).
 log(s2,'alive',info,400).
 log(s2,'alive',info,600).
-log(s2,'alive',info,800).
+% log(s2,'alive',info,800).
 log(s2,'alive',info,1000).
 log(s2,'error in processing request from s1',err,967).
 
@@ -54,8 +54,9 @@ log(s5,'error',err,975).
 
 % interaction(InvokingInstance,InvokedInstance,StartTime,EndTime)
 interaction(s1,s2,960,990).
+interaction(s1,s6,900,910).
 interaction(s2,s3,949,970).
-interaction(s2,s4,953,970).
+interaction(s3,s4,953,970).
 interaction(s5,s4,952,974).
 
 
@@ -70,18 +71,44 @@ explain(Event, Explanation) :-
     heartbeat(P),
     explain(P, [Event], [], Explanation).
 
-explain(P, [E|Evs], Explained, [x(E,log(SId,bot,emerg,FailTime))|Explanation]) :- 
-    E = log(SId,Msg,Sev,Time), 
-    FailTime is Time - P,
-    findall(X, logsBetween(P,SId,X,FailTime,Time), []),
-    explain(P, Evs, [log(SId,Msg,Sev,Time)|Explained], Explanation).
-explain([], _, []).
+% internal crash
+explain(P, [E|Evs], Explained, [x(E,F)|Explanation]) :- 
+    \+ member(E,Explained), E = log(SId,_,_,Time), 
+    FailTime is Time - P, 
+    findall(X, logsBetween(SId,X,FailTime,Time), []),
+    F = log(SId,'unexpected crash',emerg,FailTime),
+    explain(P, [F|Evs], [E|Explained], Explanation).
+% % invoked service never started
+explain(P, [E|Evs], Explained, [x(E,F)|Explanation]) :-
+    \+ member(E,Explained), E = log(SId,_,_,Time), 
+    interaction(SId,SId1,Start,_), Start < Time,
+    findall(X, log(SId1,X,_,_), []),
+    F = log(SId1,noStart,warn,0),
+    explain(P, Evs, [E|Explained], Explanation).
+% crash of invoked service
+%% %TODO: between(Low,Hi,X)
+%%%%%%%%%%%%%%%%%%%%%%%%
+% error in invoked service 
+explain(P, [E|Evs], Explained, [x(E,F)|Explanation]) :-
+    \+ member(E,Explained), E = log(SId,_,_,Time), 
+    interaction(SId,SId1,Start,End), Start < Time,
+    log(SId1,M,Sev,Time1), Time1 > Start, Time1 < End,
+    severity(Sev,A), severity(warn,W), A < W,
+    F = log(SId1,M,Sev,Time1),
+    explain(P, [F|Evs], [E|Explained], Explanation).
+% SPECIAL CASE: explaining crashes not due to interactions
+explain(P, [E|Evs], Explained, [x(E,F)|Explanation]) :-
+    \+ member(E,Explained), E = log(SId,_,emerg,Time), 
+    F = log(SId,'internal crash',emerg,Time),
+    explain(P, Evs, [E|Explained], Explanation).
+
+explain(_, [], _, []).
 
 logsBetween(SId,log(SId,_,_,XTime),FailTime,Time) :-
     log(SId,_,_,XTime),
-    XTime > Time - P, XTime < Time.
+    XTime > FailTime, XTime < Time.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%% Run Explainer %%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%
-all_explanations(log(s1,'error',err,999),E).
+%all_explanations(log(s1,'error',err,999),E).
