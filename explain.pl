24f6ed20de82349841469a6c6275d253fb42e360
Stefano <stefano.forti92@gmail.com>
2021-07-13 14:06:35 +0200

    neew version

--- explain.pl
+++ explain.pl
@@ -16,12 +16,12 @@ causedBy(log(S,T,_,_),[X]) :-
     interaction(S,S2,Ts,Te), Ts < T, heartbeat(P), 
     T0 is Ts-P, T1 is Te + P, log(S2,_,_,_),
     \+ (log(S2,U,_,_), log(S2,V,_,_), dif(U,V), T0=<U, V=<T1, V-U=<P),
-    X = crash(S2,T0,T1). % log(S2,Tx,⊥,emerg).
+    X = crash(S2,T0,T1). 
 %internal crash
 causedBy(log(S,T,_,_),[X]) :-
     heartbeat(P), T0 is T-P, T0>0,
     \+ (log(S,U,_,_), T0 =< U, U < T),
-    X = crash(S,T0,T). % log(S,T0,⊥,emerg).
+    X = crash(S,T0,T). 
 %base case
 causedBy(_,[]).
 
