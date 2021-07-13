c85cf3415392e158fde578749682e26d158b6ba4
Stefano <stefano.forti92@gmail.com>
2021-07-13 13:24:26 +0200

    reordered clauses

--- explain.pl
+++ explain.pl
@@ -1,18 +1,6 @@
 :-consult('data.pl').
 :-set_prolog_flag(last_call_optimisation, true).
 
-%internal crash
-causedBy(log(S,T,M,_),[X]) :-
-    dif(M,⊥),
-    heartbeat(P), T0 is T-P, T0>0,
-    \+ (log(S,U,_,_), T0 =< U, U < T),
-    X = log(S,T0,⊥,emerg).
-%crash of invoked service
-causedBy(log(S,T,_,_),[X]) :-
-    interaction(S,S2,Ts,Te), Ts < T, heartbeat(P), 
-    \+ (log(S2,U,_,_), log(S2,V,_,_), dif(U,V), Ts-P=<U, V=<Te+P, V-U=<P),
-    Tx is Ts-P,
-    X=log(S2,Tx,⊥,emerg).
 %error of invoked service
 causedBy(log(S,T,_,_),[X|Xs]) :-
     interaction(S,S2,Ts,Te), Ts < T,
@@ -24,6 +12,18 @@ causedBy(log(S,T,_,_),[X]) :-
     interaction(S,S2,Ts,_), Ts < T,
     \+ log(S2,_,_,_),
     X=log(S2,0,nostart,warn).
+%crash of invoked service
+causedBy(log(S,T,_,_),[X]) :-
+    interaction(S,S2,Ts,Te), Ts < T, heartbeat(P), 
+    \+ (log(S2,U,_,_), log(S2,V,_,_), dif(U,V), Ts-P=<U, V=<Te+P, V-U=<P),
+    Tx is Ts-P,
+    X=log(S2,Tx,⊥,emerg).
+%internal crash
+causedBy(log(S,T,M,_),[X]) :-
+    dif(M,⊥),
+    heartbeat(P), T0 is T-P, T0>0,
+    \+ (log(S,U,_,_), T0 =< U, U < T),
+    X = log(S,T0,⊥,emerg).
 %base case
 causedBy(_,[]).
 
