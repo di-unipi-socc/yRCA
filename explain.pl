6642e8ac5e37b19ac9679dd3b40115d589da8d58
Stefano <stefano.forti92@gmail.com>
2021-07-16 09:57:50 +0200

    new interaction predicate

--- explain.pl
+++ explain.pl
@@ -29,3 +29,7 @@ lte(S1,S2) :- severity(S1,A), severity(S2,B), A=<B.
 %:-causedBy(log(s1,999,'error',err),X).
 %:-causedBy(log(s0,10000,_,_),X).
 
+% interaction(S1,S2,Ts,Te) :-
+%     log(S1,Ts,(_,ID),_), dif(ID,nil),
+%     log(S2,Tm,(_,ID),_), Tm > Ts,
+%     log(S1,Te,(_,ID),_), Te > Tm.
