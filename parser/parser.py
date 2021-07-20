5430288147108b5a87f2272fb18c9550ef30a48c
jsoldani <jsoldani@gmail.com>
2021-07-20 13:22:20 +0200

    templating chaos-echo logs

--- parser/parser.py
+++ parser/parser.py
@@ -1,38 +1,24 @@
-import json
 from parser.templates.chaos_echo import parse
 
 def parseEvents(eventLogs,targetFile):
     # open source/target files 
-    jsonLogs = open(eventLogs,"r")
+    logs = open(eventLogs,"r")
     prologLogs = open(targetFile,"w")
-    for jsonLog in jsonLogs:
+    for log in logs:
         # parsing log fields
-        log = json.loads(jsonLog)
-
-        containerName = log["container_name"]
-        containerId = log["container_id"]
-        try:
-            event = log["event"]
-            severity = log["severity"]
-            timestamp = log["timestamp"]
-        except:
-            # handle Logstash's grok parse failures
-            event = log["message"]
-            severity = "INFO"
-            timestamp = log["@timestamp"]
-
-        prologLogs.write(logFact(containerName,containerId,event,timestamp,severity))
+        event = parse(log)
+        # write prolog corresponding fact
+        prologLogs.write(logFact(event))
 
     # close source/target files
-    jsonLogs.close()
+    logs.close()
     prologLogs.close()
 
-def logFact(containerName,containerId,event,timestamp,severity):
+def logFact(event):
     fact = "log("
-    #fact += containerName + ","
-    fact += containerId + ","
-    fact += "'" + parse(event) + "',"
-    fact += timestamp + ","
-    fact += severity
+    fact += event["instance"] + ","
+    fact += event["timestamp"] + ","
+    fact += "'" + event["message"].replace("'","").replace("/","").replace("\\","") + "',"
+    fact += event["severity"]
     fact += ").\n"
     return fact
--- parser/templates/chaos_echo.py
+++ parser/templates/chaos_echo.py
@@ -1,3 +1,16 @@
-def parse(event):
-    t = event.replace("'","").replace("/","").replace("\\","")
-    return t
+import json 
+
+def parse(jsonLog):
+    log = json.loads(jsonLog)
+    event = {}
+    event["instance"] = log["container_name"]
+    try:
+        event["message"] = log["event"]
+        event["severity"] = log["severity"]
+        event["timestamp"] = log["timestamp"]
+    except:
+        # handle Logstash's grok parse failures
+        event["message"] = log["message"]
+        event["severity"] = "INFO"
+        event["timestamp"] = log["@timestamp"]
+    return event
