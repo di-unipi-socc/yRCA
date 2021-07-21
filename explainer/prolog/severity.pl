24e12ce279897977098a407bbcd9817f517aa50b
jsoldani <jsoldani@gmail.com>
2021-07-21 15:54:27 +0200

    ssyslog severity in prolog

---
+++ explainer/prolog/severity.pl
@@ -0,0 +1,9 @@
+% Severity levels as per Syslog protocol: https://datatracker.ietf.org/doc/html/rfc5424
+severity(emerg, 0). 
+severity(alert, 1).
+severity(crit, 2).
+severity(err, 3).
+severity(warn, 4).
+severity(notice, 5).
+severity(info, 6).
+severity(debug, 7).
