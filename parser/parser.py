fa64759a4e6a22d7de0f52ae4946a6150e163267
jsoldani <jsoldani@gmail.com>
2021-07-22 09:55:46 +0200

    minor fix (ids as strings)

--- parser/parser.py
+++ parser/parser.py
@@ -35,11 +35,11 @@ def logFact(event):
 # (it takes as input a "Message" -> see module "message.py")
 def generateMessage(msg):
     if msg.type == MessageType.CLIENT_SEND:
-        return "sendTo(" + msg.parameters.service + "," + msg.parameters.requestId + ")"
+        return "sendTo(" + msg.parameters.service + ",'" + msg.parameters.requestId + "')"
     if msg.type == MessageType.CLIENT_RECEIVE:
-        return "answerFrom(" + msg.parameters.service + "," + msg.parameters.requestId + ")"
+        return "answerFrom(" + msg.parameters.service + ",'" + msg.parameters.requestId + "')"
     if msg.type == MessageType.SERVER_RECEIVE:
-        return "received(" + msg.parameters.requestId + ")"
+        return "received('" + msg.parameters.requestId + "')"
     if msg.type == MessageType.SERVER_SEND:
-        return "answeredTo(" + msg.parameters.requestId + ")"
+        return "answeredTo('" + msg.parameters.requestId + "')"
     return "other"
