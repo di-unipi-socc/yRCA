87df3623c96ed78487da6a7939ab3754ca990472
jsoldani <jsoldani@gmail.com>
2021-07-22 11:31:34 +0200

    first version

--- explainer/explainer.py
+++ explainer/explainer.py
@@ -1,2 +1,19 @@
-def explain(event,logged_events):
-    print("TODO: explain(" + event + "," + logged_events + ")")
+from pyswip import Prolog
+def explain(event,appLogs):
+    # create Prolog reasoner
+    reasoner = Prolog()
+    
+    # load knowledge base
+    reasoner.consult("explainer/prolog/severity.pl")
+    reasoner.consult("explainer/prolog/explain.pl")
+    reasoner.consult(appLogs)
+    
+    # read event to explain
+    eventFile = open(event,"r")
+    eventToExplain = eventFile.readline() # read line corresponding to event
+    eventFile.close()
+    eventToExplain = eventToExplain[:len(eventToExplain)-2] # remove "." and "\n" at the end
+    
+    # run Prolog reasoner to find (and return) root causes
+    rootCauses = list(reasoner.query("causedBy(" + eventToExplain + ",C)"))
+    return rootCauses
--- explainer/prolog/explain.pl
+++ explainer/prolog/explain.pl
@@ -1,35 +1,35 @@
 :-set_prolog_flag(last_call_optimisation, true).
 
 %error of invoked service
-causedBy(log(S,T,_,_),[X|Xs]) :-
-    interaction(S,S2,Ts,Te), Ts < T,
-    log(S2,U,M,Sev), lte(Sev,warning), Ts<U, U<Te,
-    X=log(S2,U,M,Sev),
+causedBy(log(_,S,T,_,_),[X|Xs]) :-
+    interaction(S,S2,Ts,Te), heartbeat(P), Ts < T-P, % Ts < T, 
+    log(N,S2,U,M,Sev), lte(Sev,warning), Ts<U, U<Te,
+    X=log(N,S2,U,M,Sev),
     causedBy(X,Xs).
 %invoked service never started
-causedBy(log(S,T,_,_),[X]) :-
-    interaction(S,S2,Ts,_), Ts < T,
-    \+ log(S2,_,_,_),
-    X=log(S2,0,nostart,warning).
+causedBy(log(_,S,T,_,_),[X]) :-
+    log(_,S,Ts,sendTo(N2,_),_), Ts < T,
+    \+ log(N2,_,_,_,_),
+    X = neverStarted(N2).
 %crash of invoked service
-causedBy(log(S,T,_,_),[X]) :-
+causedBy(log(_,S,T,_,_),[X]) :-
     interaction(S,S2,Ts,Te), Ts < T, heartbeat(P), 
-    T0 is Ts-P, T1 is Te + P, log(S2,_,_,_),
-    \+ (log(S2,U,_,_), log(S2,V,_,_), dif(U,V), T0=<U, V=<T1, V-U=<P),
-    X = crash(S2,T0,T1). % TODO: could be Ts, Te?
+    T0 is Ts-P, T1 is Te + P, log(N,S2,_,_,_),
+    findall(1,(log(_,S2,U,_,_), log(_,S2,V,_,_), dif(U,V), T0=<U, V=<T1, V-U=<P),[]),
+    %\+ (log(_,S2,U,_,_), log(_,S2,V,_,_), dif(U,V), T0=<U, V=<T1, V-U=<P),
+    X = crash(N,S2,T0,T1). % TODO: could be Ts, Te?
 %internal crash
-causedBy(log(S,T,_,_),[X]) :-
+causedBy(log(N,S,T,_,_),[X]) :-
     heartbeat(P), T0 is T-P, T0>0,
-    \+ (log(S,U,_,_), T0 =< U, U < T),
-    X = crash(S,T0,T). 
+    findall(1,(log(_,S,U,_,_), T0 =< U, U < T),[]),
+    %\+ (log(_,S,U,_,_), T0 =< U, U < T),
+    X = crash(N,S,T0,T). 
 %base case
 causedBy(_,[]).
 
 lte(S1,S2) :- severity(S1,A), severity(S2,B), A=<B.
-%:-causedBy(log(s1,999,'error',err),X).
-%:-causedBy(log(s0,10000,_,_),X).
 
-% interaction(S1,S2,Ts,Te) :-
-%     log(S1,Ts,(_,ID),_), 
-%     log(S2,Tm,(_,ID),_), Tm > Ts,
-%     log(S1,Te,(_,ID),_), Te > Tm.
+interaction(S1,S2,Ts,Te) :-
+    log(N1,S1,Ts,sendTo(N2,Id),_), 
+    log(N2,S2,_,received(Id2),_), Id = Id2,
+    log(N1,S1,Te,answerFrom(N2,Id),_), Te > Ts.
