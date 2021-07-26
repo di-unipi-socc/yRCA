fec79d8c1c2ce7640cde47c0b139a9c9d347b632
jsoldani <jsoldani@gmail.com>
2021-07-26 14:45:48 +0200

    printing 'log'

--- post_processor/post_processor.py
+++ post_processor/post_processor.py
@@ -1,2 +1,9 @@
-def post_process():
-    print("TODO: post-processing")
+def post_process(rootCauses,applicationLogs):
+    print("TODO: post-processing (based on " + applicationLogs + ")\n")
+    print("N SOLS: ", len(rootCauses))
+    for rc in rootCauses:
+        # each rc is a list itself
+        for c in rc["C"]:
+            print(c, end="->")
+        print("END") 
+        #print(rc["C"])
