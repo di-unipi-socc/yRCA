8fc0b1ea3a6ebcb1212a18c9f27bd616fcce4fcb
jsoldani <jsoldani@gmail.com>
2021-09-28 14:41:03 +0000

    run

--- data/experiments/sock-echo/run.sh
+++ data/experiments/sock-echo/run.sh
@@ -7,24 +7,28 @@ curl https://raw.githubusercontent.com/di-unipi-socc/chaos-echo/main/deploy/exam
 curl https://raw.githubusercontent.com/di-unipi-socc/chaos-echo/main/generate_workload.sh > generate_workload.sh
 chmod ugo+x generate_workload.sh
 
-for s in $serviceList; do
+# s="ORDERS" 			# uncomment for single run 
+for s in $serviceList; do 	# comment for single run
     # generate docker-compose file with single point of failure 
     # (service s failing, all other services s1 not failing)
     cp docker-compose.yml docker-compose.yml.original
-    sed -i "s/${s}_FAIL/100/" docker-compose.yml
+    sed -i "s/${s}_FAIL/50/" docker-compose.yml
     for s1 in $serviceList ; do
         if [ $s != $s1 ]; then
             sed -i "s/${s1}_FAIL/0/" docker-compose.yml 
         fi 
     done
 
+    echo ""
+    cat  docker-compose.yml
+    echo ""
     # launch & load deployment
     docker stack deploy -c docker-compose.yml sockecho
-    sleep 60
-    ./generate_workload.sh -d 30 -p 0.1
-    sleep 60
+    sleep 30
+    ./generate_workload.sh -d 15 -p 0.5
+    sleep 30
     docker stack rm sockecho
 
     # restore original docker file
     mv docker-compose.yml.original docker-compose.yml
-done
+done 				#comment for single run
