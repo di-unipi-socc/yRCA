7600f0e8e122c4a69aa5666aff132b1cbbe726f2
Jacopo Soldani <jsoldani@gmail.com>
2023-05-30 08:26:00 +0000

    minor fix (python vs python3)

--- capsule/run_all.sh
+++ capsule/run_all.sh
@@ -11,13 +11,13 @@ echo -e "${C}==> EXAMPLE 1: FAILURE INJECTED ON SERVICE 'orders'${NC}"
 echo -e "${C}(src: https://github.com/di-unipi-socc/yRCA/tree/main/data/examples/sock-echo/orders-fail)${NC}"
 echo ""
 echo -e "${C}* Case 1.1: Failure directly observed on 'orders'${NC}"
-python3 yrca.py data/examples/sock-echo/orders-fail/event-orders.log data/examples/sock-echo/orders-fail/all.log data/templates/chaos-echo.yml
+python yrca.py data/examples/sock-echo/orders-fail/event-orders.log data/examples/sock-echo/orders-fail/all.log data/templates/chaos-echo.yml
 echo ""
 echo -e "${C}* Case 1.2: Failure observed on 'frontend'${NC}"
-python3 yrca.py data/examples/sock-echo/orders-fail/event-frontend.log data/examples/sock-echo/orders-fail/all.log data/templates/chaos-echo.yml
+python yrca.py data/examples/sock-echo/orders-fail/event-frontend.log data/examples/sock-echo/orders-fail/all.log data/templates/chaos-echo.yml
 echo ""
 echo -e "${C}* Case 1.3: Failure observed on 'edgeRouter'${NC}"
-python3 yrca.py data/examples/sock-echo/orders-fail/event-edgeRouter.log data/examples/sock-echo/orders-fail/all.log data/templates/chaos-echo.yml
+python yrca.py data/examples/sock-echo/orders-fail/event-edgeRouter.log data/examples/sock-echo/orders-fail/all.log data/templates/chaos-echo.yml
 
 # Example 2: Shipping failing
 echo ""
@@ -25,7 +25,7 @@ echo -e "${C}==> EXAMPLE 2: FAILURE INJECTED ON SERVICE 'shipping'${NC}"
 echo -e "${C}(src: https://github.com/di-unipi-socc/yRCA/tree/main/data/examples/sock-echo/shipping-fail)${NC}"
 echo ""
 echo -e "${C}* Case 2.1: Failure observed on 'edgeRouter'${NC}"
-python3 yrca.py data/examples/sock-echo/shipping-fail/event-edgeRouter.log data/examples/sock-echo/shipping-fail/all.log data/templates/chaos-echo.yml
+python yrca.py data/examples/sock-echo/shipping-fail/event-edgeRouter.log data/examples/sock-echo/shipping-fail/all.log data/templates/chaos-echo.yml
 
 # Example 3: Users and Shipping both failing
 echo ""
@@ -33,7 +33,7 @@ echo -e "${C}==> EXAMPLE 3: FAILURE INJECTED ON BOTH 'users' and 'shipping'${NC}
 echo -e "${C}(src: https://github.com/di-unipi-socc/yRCA/tree/main/data/examples/sock-echo/users-shipping-fail)${NC}"
 echo ""
 echo -e "${C}* Case 3.1: Failure observed on 'edgeRouter'${NC}"
-python3 yrca.py data/examples/sock-echo/users-shipping-fail/event.log data/examples/sock-echo/users-shipping-fail/all.log data/templates/chaos-echo.yml
+python yrca.py data/examples/sock-echo/users-shipping-fail/event.log data/examples/sock-echo/users-shipping-fail/all.log data/templates/chaos-echo.yml
 
 # Example 4: Timeout cascade
 echo ""
@@ -41,5 +41,5 @@ echo -e "${C}==> EXAMPLE 4: SERVICE 'shipping' MADE UNREACHABLE${NC}"
 echo -e "${C}(src: https://github.com/di-unipi-socc/yRCA/blob/main/data/examples/sock-echo/timeout-cascade)${NC}"
 echo ""
 echo -e "${C}* Case 4.1: Timeout expiring on 'edgeRouter'${NC}"
-python3 yrca.py data/examples/sock-echo/timeout-cascade/event.log data/examples/sock-echo/timeout-cascade/all.log data/templates/chaos-echo.yml
+python yrca.py data/examples/sock-echo/timeout-cascade/event.log data/examples/sock-echo/timeout-cascade/all.log data/templates/chaos-echo.yml
 
